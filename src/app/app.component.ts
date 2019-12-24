0ebe5b8120c736f1ffc7b42fc93ab06c1451022e
Ramkumar.K <ramkumar.k@payoda.com>
2019-12-24 16:25:23 +0530

    Implemented login and register

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,24 @@
-import { Component } from '@angular/core';
-@Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
-})
-export class AppComponent {}
+﻿import { Component } from '@angular/core';
+import { Router } from '@angular/router';
+
+import { AuthenticationService } from './_services';
+import { User } from './_models';
+
+import './_content/app.less';
+
+@Component({ selector: 'app', templateUrl: 'app.component.html' })
+export class AppComponent {
+    currentUser: User;
+
+    constructor(
+        private router: Router,
+        private authenticationService: AuthenticationService
+    ) {
+        this.authenticationService.currentUser.subscribe(x => this.currentUser = x);
+    }
+
+    logout() {
+        this.authenticationService.logout();
+        this.router.navigate(['/login']);
+    }
+}
--- src/app/quiz/quiz-home/state-machine/quiz-list-schema.ts
+++
@@ -1,9 +0,0 @@
-export interface quizListSchema {
-    states: {
-      boot: {};
-      loggedOut: {};
-      loggedIn: {};
-      requestErr: {};
-      loading: {};
-    };
-  }
